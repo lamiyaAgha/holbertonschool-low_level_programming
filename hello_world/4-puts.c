2273c93b020f37fc5803559b5cf9c74551a0c58f
root <aghayevalamiya7@gmail.com>
2023-10-15 06:32:24 -0700

    commit

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,5 +1,13 @@
 #include <stdio.h>
 
+/**
+ * main - Entry point of the program
+ *
+ * This function is the entry point of the program and it
+ * prints a message to the standard output.
+ *
+ * Return: 0 on success
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
