a848a27adcfbbb3cbf781d3ed5b66d66ef34c812
root <aghayevalamiya7@gmail.com>
2023-10-15 06:35:00 -0700

    commit

---
+++ hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * This function is the entry point of the program and it
+ * prints a message to the standard output.
+ *
+ * Return: 0 on success
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
