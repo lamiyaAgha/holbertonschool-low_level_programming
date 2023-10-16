3f0598b7d114843f8bc400c416a31cf1ac8cc213
root <aghayevalamiya7@gmail.com>
2023-10-16 13:05:06 -0700

    commit

--- variables_if_else_while/9-print_comb.c
+++ variables_if_else_while/9-print_comb.c
@@ -2,18 +2,24 @@
 
 int main(void)
 {
-  int i,j;
-    for (i = '0'; i <= '9'; i++)
+    int i = '0';
+
+    while (i <= '9')
     {
-      for (j = '0'; j <= '9'; j++)
+        int j = '0';
+
+        while (j <= '9')
         {
             putchar(i);
-            putchar(", ");
-            putchar(j);
-            putchar('\n');
+            putchar((j == '9') ? '\n' : ',');
+            if (j != '9')
+                putchar(' ');
+
+            j++;
         }
+
+        i++;
     }
 
-    return (0);
+    return 0;
 }
-
