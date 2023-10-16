264d7476907f3f5e6eae129339943fe51cb22a60
root <aghayevalamiya7@gmail.com>
2023-10-16 12:02:33 -0700

    commit

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main(void)
+{
+    char letter = 'a';
+
+    // Print lowercase alphabet
+    while (letter <= 'z')
+    {
+        putchar(letter);
+        letter++;
+    }
+
+    // Reset letter to 'A' for uppercase
+    letter = 'A';
+
+    // Print uppercase alphabet
+    while (letter <= 'Z')
+    {
+        putchar(letter);
+        letter++;
+    }
+
+    // Print a newline character
+    putchar('\n');
+
+    return 0;
+}
