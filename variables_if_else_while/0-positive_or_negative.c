53c258ad376b35f918bb80b66f0093aef4471675
root <aghayevalamiya7@gmail.com>
2023-10-16 10:27:23 -0700

    commit

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -1,7 +1,6 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-
 /**
  * main - Entry point
  *
@@ -13,12 +12,9 @@
 int main(void)
 {
     int n;
-
     srand(time(0));
     n = rand() - RAND_MAX / 2;
-
     printf("%d is ", n);
-
     if (n > 0)
     {
         printf("positive");
@@ -31,9 +27,7 @@ int main(void)
     {
         printf("negative");
     }
-
     printf("\n");
-
     return (0);
 }
 
