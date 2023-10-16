a80718dcf97c8620bd0bad6775eebf230aa363ad
root <aghayevalamiya7@gmail.com>
2023-10-16 12:22:42 -0700

    commit

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main(void)
+{
+  int number = 0;
+  while (number <= 9)
+    {
+      printf("%d", number);
+      number++;
+    }
+  printf("\n");
+  return (0);
+}
