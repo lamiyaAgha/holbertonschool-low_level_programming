78deb0ec779edd973b6e737a2fb337abe57b02a9
root <aghayevalamiya7@gmail.com>
2023-10-16 11:51:58 -0700

    commit

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -13,20 +13,19 @@ int main(void)
 
     printf("Last digit of %d is ", n);
 
-    int lastDigit = n % 10;
-
-    if (lastDigit > 5)
+    if (n % 10 > 5)
     {
-        printf("and is greater than 5\n");
+      printf("%d and is greater than 5", n % 10);
     }
-    else if (lastDigit == 0)
+    else if (n % 10 == 0)
     {
-        printf("and is 0\n");
+      printf("%d and is 0", n % 10);
     }
-    else
+    else if ((n % 10 < 6) && (n % 10 != 0))
     {
-        printf("and is less than 6 and not 0\n");
+      printf("%d and is less than 6 and not 0", n % 10);
     }
+    printf("\n")
 
     return (0);
 }
