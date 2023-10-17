be076c3e591c80d3431f28f05d6f5b36d83f7510
root <aghayevalamiya7@gmail.com>
2023-10-17 10:28:18 -0700

    commit

--- variables_if_else_while/8-print_base16.c
+++ variables_if_else_while/8-print_base16.c
@@ -1,21 +1,24 @@
 #include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: This program generates a random number and
+ * determines whether it is positive, zero, or negative.
+ *
+ * Return: Always 0
+ */
 
 int main(void)
 {
-    char hexDigit;
-
-    /* Print numbers '0' to '9'*/
-    for (hexDigit = '0'; hexDigit <= '9'; hexDigit++)
-    {
-        putchar(hexDigit);
-    }
-
-    for (hexDigit = 'a'; hexDigit <= 'f'; hexDigit++)
-    {
-        putchar(hexDigit);
-    }
-
-    putchar('\n');
-
-    return 0;
+char hexDigit;
+for (hexDigit = '0'; hexDigit <= '9'; hexDigit++)
+{
+putchar(hexDigit);
+}
+for (hexDigit = 'a'; hexDigit <= 'f'; hexDigit++)
+{
+putchar(hexDigit);
+}
+putchar('\n');
+return (0);
 }
