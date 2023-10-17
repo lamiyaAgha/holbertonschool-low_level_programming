f8b85ae7ac47850844492b6a2a58f175a178e390
root <aghayevalamiya7@gmail.com>
2023-10-17 10:24:36 -0700

    commit

--- variables_if_else_while/6-print_numberz.c
+++ variables_if_else_while/6-print_numberz.c
@@ -1,16 +1,22 @@
 #include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: This program generates a random number and
+ * determines whether it is positive, zero, or negative.
+ *
+ * Return: Always 0
+ */
 
 int main(void)
 {
-    int number = 0;
+int number = 0;
 
-    while (number <= 9)
-    {
-        putchar(48 + number);
-        number++;
-    }
-
-    putchar('\n');
-
-    return 0;
+while (number <= 9)
+{
+putchar(48 + number);
+number++;
+}
+putchar('\n');
+return (0);
 }
