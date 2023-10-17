cf74b5294651f944f6f1a446aae21873a3333f75
root <aghayevalamiya7@gmail.com>
2023-10-17 10:41:24 -0700

    commit

--- variables_if_else_while/9-print_comb.c
+++ variables_if_else_while/9-print_comb.c
@@ -1,18 +1,23 @@
 #include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: This program generates a random number and
+ * determines whether it is positive, zero, or negative.
+ *
+ * Return: Always 0
+ */
 
 int main(void)
 {
-    int i = '0';
-    do
-      {
-	putchar(i);
-	putchar(',');
-	putchar(' ');
-	if (i == '9')
-	  {
-	    putchar('\n');
-	  }
-	i++;
-      } while (i <= '9');
-    return 0;
+int i = '0';
+putchar(i);
+do
+{
+i++;
+putchar(',');
+putchar(' ');
+putchar(i);
+} while (i <= '8');
+return (0);
 }
