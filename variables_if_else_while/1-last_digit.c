3277180d5df4986d819273f5e26565db52d5bd66
root <aghayevalamiya7@gmail.com>
2023-10-17 09:53:15 -0700

    commit

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -1,17 +1,14 @@
 #include <stdlib.h>
 #include <time.h>
-#include <stdio.h>
+/* more headers goes there */
 
-/**
-this is betty
-*/
+/* betty style doc for function main goes there */
 int main(void)
 {
 int n;
-
 srand(time(0));
-n = rand()- RAND_MAX / 2;
-
+n = rand() - RAND_MAX / 2;
+/* your code goes there */
 printf("Last digit of %d is ", n);
 
 if (n % 10 > 5)
@@ -30,3 +27,4 @@ printf("\n");
 
 return (0);
 }
+
