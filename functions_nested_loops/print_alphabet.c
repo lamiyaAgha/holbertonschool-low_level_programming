f247dcd1d3dc10df2a05e99e649791bada523aa2
root <aghayevalamiya7@gmail.com>
2023-10-28 13:20:40 -0700

    commit

--- functions_nested_loops/print_alphabet.c
+++ functions_nested_loops/print_alphabet.c
@@ -1,14 +1,15 @@
 #include "main.h"
+#include <unistd.h>
 
 void print_alphabet(void)
 {
-    char letter = 'a';
+char letter = 'a';
 
-    while (letter <= 'z')
-    {
-        _putchar(letter);
-        letter++;
-    }
-    _putchar('\n');
+while (letter <= 'z')
+{
+_putchar(letter);
+letter++;
+}
+_putchar('\n');
 }
 
