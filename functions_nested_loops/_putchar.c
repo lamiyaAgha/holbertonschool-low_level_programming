561298c0d01bae5334d2244ca986e2d5b88e250b
root <aghayevalamiya7@gmail.com>
2023-11-09 12:15:35 -0800

    commit

--- functions_nested_loops/_putchar.c
+++ functions_nested_loops/_putchar.c
@@ -1,4 +1,13 @@
+#include <unistd.h>
+#include "main.h"
+
+/**
+ * _putchar - writes a character to the standard output
+ * c: The character to be written
+ * Return: On success, 1. On error, -1.
+ */
 int _putchar(char c)
 {
-return write(1, &c, 1); 
+return write(1, &c, 1);
 }
+
